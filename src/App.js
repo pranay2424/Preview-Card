259e4abafb16e0967b8f860d13acd84a77454e1c
pranay2424 <78260377+pranay2424@users.noreply.github.com>
2021-06-30 02:44:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import FirstFile from "./Components/stats-preview-card-component-main/library/FirstFile";
-import SecondFile from "./Components/3-column-preview-card-component-main/library/SecondFile";
-import ThirdFile from "./Components/profile-card-component-main/library/ThirdFile";
-import FourthFile from "./Components/faq-accordion-card-main/library/FourthFile"
+
 import "./App.css";
 
 
@@ -9,9 +7,6 @@ function App() {
   return (
     <div className="App">
       <FirstFile  width="100%" />
-      <SecondFile  width="100%"/>
-      <ThirdFile width="100%" />
-      <FourthFile width="100" />
     </div>
   );
 }
